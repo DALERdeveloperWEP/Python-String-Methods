751ad13bb564ce0ba69ca965841a17ca1c8e8569
DALERdeveloperWEP <cassidymlbbbbb@gmail.com>
2025-08-28 10:18:01 +0500

    02,07,13

---
+++ task07.py
@@ -0,0 +1,3 @@
+user_str = input('string: ')
+end_str = input('end string: ')
+print(user_str.endswith(end_str))
---
+++ task13.py
@@ -0,0 +1,3 @@
+start_str = input('start string: ')
+user_str = input('user str: ')
+print(user_str.startswith(user_str))
